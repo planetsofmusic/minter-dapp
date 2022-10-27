76176a8a55bcca0e652ab1abdfa26b7f86c9ed5b
justminehere <114820796+justminehere@users.noreply.github.com>
2022-10-27 15:16:45 +0200

    layers

--- backend/src/config.js
+++ backend/src/config.js
@@ -13,7 +13,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 2300,
     layersOrder: [
       { name: "ABunnyGalaxy" },
       { name: "BunnyBody" },
